a5561ca03e9268db81306e0454fafcccbcfb637f
joshuagame <joshuagame@gmail.com>
2020-08-05 12:02:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * express-requid
+ * Express.js request id middleware
+ *
+ * Copyright(c) 2020 The express-requid authors.
+ *
+ * Use of this source code is governed by a MIT license.
+ * You can find the MIT license term for this source code
+ * in the LICENSE file.
+ *
+ * index.js
+ *
+ * Authors:
+ *      Luca Stasio <joshuagame@gmail.com>
+ */
+
+module.exports = require('./lib/express-requid');
