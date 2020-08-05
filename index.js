4ef0afec0b3428a5b313bfa9a0faa21ec1d2448b
joshuagame <joshuagame@gmail.com>
2020-08-05 15:23:10 +0200

    example added

--- index.js
+++ index.js
@@ -11,28 +11,4 @@
  * index.js
  */
 
-// module.exports = require('./lib/express-requid');
-Promise = require('bluebird');
-const express = require('express');
-const requid = require('./lib/express-requid');
-
-const app = express();
-
-app.use(requid());
-
-app.get('/', function (req, res, next) {
-    res.json({
-        requestId: req.rid
-    });
-});
-
-const server =
-    app.listen(3000, '0.0.0.0')
-        .on('listening', () => {
-            console.log(Number.MAX_SAFE_INTEGER);
-            console.log(`server listening on ${server.address().address}:${server.address().port}`);
-        })
-        .on('error', (err) => {
-            console.error(err);
-            process.exit(1);
-        });
+module.exports = require('./lib/express-requid');
