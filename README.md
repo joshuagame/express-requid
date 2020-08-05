3a3d958bd86ab2542bb727ad4ac04e8662d34e1b
joshuagame <joshuagame@gmail.com>
2020-08-05 15:04:01 +0200

    rewrite exported middleware function

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 An Express.js middleware to generate unique id for incoming requests. The generated id will have a prefix and a sequence number.
 
 In case a request has the request id header header valued, then the middleware will use it.
+
+```js
+const express = require('express');
+const requid = require('express-requid');
+
+app.use(requid());
+
+app.get('/', function(req, res, next) {
+    res.json({
+        requestId: req.rid
+    });
+});
+
+app.listen(3000, () => {
+    console.log(`server listening on ${server.address().address}:${server.addredd().port}`);
+});
+```
+
+To test the sample:
+```bash
+$ curl localhost:3000
+{"requestId":"redrock/8aa8fcf4a0af9d153f1f0772-0000000000000001"}
+```
--- index.js
+++ index.js
@@ -11,4 +11,27 @@
  * index.js
  */
 
-module.exports = require('./lib/express-requid');
+// module.exports = require('./lib/express-requid');
+Promise = require('bluebird');
+const express = require('express');
+const requid = require('./lib/express-requid');
+
+const app = express();
+
+app.use(requid());
+
+app.get('/', function (req, res, next) {
+    res.json({
+        requestId: req.rid
+    });
+});
+
+const server =
+    app.listen(3000, '0.0.0.0')
+        .on('listening', () => {
+            console.log(`server listening on ${server.address().address}:${server.address().port}`);
+        })
+        .on('error', (err) => {
+            console.error(err);
+            process.exit(1);
+        });
